8bda09db6aff97ce829f4f44372f28cb92f4ebbc
Tomusdrw <tomusdrw@gmail.com>
2013-01-08 15:59:14 +0100

    Require.js and some base libs.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+requirejs.config({
+	paths : {
+		'bootstrap' : 'vendor/bootstrap',
+		'underscore' : 'vendor/underscore',
+		'backbone' : 'vendor/backbone',
+		'backbone.storage' : 'vendor/backbone.webStorage'
+	},
+	map : {
+		'*' : {
+			'_' : 'underscore'
+		}
+	},
+	shim : {
+		'underscore' : {
+			exports : '_'
+		},
+		'backbone' : {
+			deps : ['underscore', 'jquery'],
+			exports : 'Backbone'
+		},
+		'backbone.storage' : {
+			deps : ['backbone'],
+			exports : 'Backbone'
+		},
+		'bootstrap' : {
+			deps : ['jquery']
+		}
+	}
+});
+require(['jquery'], function ($) {
+	
 
+});
